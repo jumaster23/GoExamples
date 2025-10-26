dd6bdb1908753148726e70765638e8b431ce6a73
jumaster23 <jesusmjuma23@hotmail.com>
2025-10-25 22:56:21 -0400

    parte4 composicion

---
+++ programacion_objetos/parte4 composicion/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"project/pkg/customer"
+	"project/pkg/invoice"
+	"project/pkg/invoiceitem"
+)
+
+
+
+func main() {
+	i := invoice.New(
+		"Colombia", 
+		"Popayan",
+		customer.New("alejandro", "calle 123", "46464"),
+		[]invoiceitem.Item{
+			invoiceitem.New(1, "curso de go", 12.34),
+			invoiceitem.New(2, "curso de Poo con go", 54.23),
+			invoiceitem.New(3, "curso de testing con Go", 90.00),
+		},
+	)
+
+	i.Settotal()
+	fmt.Printf("%+v", i)
+}
---
+++ programacion_objetos/parte4 composicion/pkg/invoice/invoice.go
@@ -0,0 +1,35 @@
+package invoice
+
+import (
+	"project/pkg/customer"
+	"project/pkg/invoiceitem"
+)
+
+type Invoice struct {
+	country string
+	ciudad  string
+	total   float64
+	client  customer.Customer
+	items   []invoiceitem.Item
+}
+
+// return a new invoice
+func New(country, ciudad string, client customer.Customer, items []invoiceitem.Item)Invoice{
+	return Invoice{
+		country: country,
+		ciudad: ciudad,
+		client: client,
+		items: items,
+	}
+}
+
+
+
+
+
+// settotal is the setter of Invoice.total
+func (i *Invoice) Settotal(){
+	for _, item := range i.items{
+		i.total += item.Value()
+	}
+}
