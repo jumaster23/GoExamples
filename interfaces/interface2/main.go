fd51ec4e045bef28da3e2157f5b8ed07c90b2b8a
jumaster23 <jesusmjuma23@hotmail.com>
2025-10-27 16:20:47 -0400

    Aprendiendo interface en Go

---
+++ interfaces/interface2/main.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+type Greeter interface {
+	Greet()
+}
+
+type Person struct {
+	Name string
+}
+
+func (p Person) Greet() {
+	fmt.Printf("Hola soy %s", p.Name)
+}
+
+type Text string
+
+func (t Text) Greet() {
+	fmt.Printf("Hola soy %s", t)
+}
+
+func (t Text) Bye() {
+	fmt.Printf("Adios soy %s", t)
+}
+
+
+type GreeterByer interface{
+	Greeter
+	Byer
+}
+
+func (p Person) String() string{
+	return "Mi nombre es: " + p.Name
+}
+
+type Byer interface{
+	Bye()
+}
+func (p Person) Bye() {
+	fmt.Printf("Adios %s", p.Name)
+}
+
+func All(gbs ...GreeterByer){
+	for _, gs := range gbs{
+		gs.Greet()
+		gs.Bye()
+	}
+}
+
+func main() {
+	//var g Greeter = Person{"Alejandro"}
+	//var g Greeter = Text("Manuel")
+	p := Person{Name: "Alejandro"}
+	//var t Text = "Daisy"
+
+	//All(p, t)
+	fmt.Println(p)
+	
+}
